fc8359a5182a9a229e80e1ac565a69cdaa004fff
triders <tim.triders1@gmail.com>
2022-01-20 19:30:28 +0300

    LoginPage Object now using in test_main_page case

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,10 +4,10 @@ from pages.locators import LoginPageLocators
 
 class LoginPage (BasePage):
 
-    # def should_be_login_page(self):
-    #     self.should_be_login_url()
-    #     self.should_be_login_form()
-    #     self.should_be_register_form()
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
 
     def should_be_login_url(self):
         current_url = self.browser.current_url
