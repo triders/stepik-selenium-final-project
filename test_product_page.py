1d3322f204e409ffd8437aa46e95b5706eec22e5
triders <tim.triders1@gmail.com>
2022-01-25 16:25:35 +0300

    returned back parametrization to test_guest_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -47,12 +47,18 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message_before_product_added_to_basket()
 
 
+base_url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+url_list = [base_url + "?promo=offer" + str(n) for n in range(0, 10)]  # promo=offer7 has bug, will mark it as xfail
+url_list_7th_is_xfail = [pytest.param(url, marks=pytest.mark.xfail) if url[-1] == "7" else url for url in url_list]
+
+
+@pytest.mark.parametrize('url', url_list_7th_is_xfail)
 @pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-    url = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+def test_guest_can_add_product_to_basket(browser, url):
     page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
+    page.solve_quiz_and_get_code()
     page.should_be_product_name_in_added_to_basket_success_message()
     page.should_be_basket_value_equal_to_product_price()
 
